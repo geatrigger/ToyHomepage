fc1303dca26f739eb13cb807f746985e2f52a791
gckim <geatrigger@gmail.com>
2020-05-23 19:31:24 +0900

    Fix production config

--- documents/pm2.md
+++ documents/pm2.md
@@ -12,5 +12,8 @@ pm2 ecosystem
 * deploy
 ```
 // git 커밋을 푸쉬먼저 해야함
+// ssh key에 암호걸려있으면 실패함
+pm2 deploy ecosystem.config.js production setup
 pm2 deploy ecosystem.config.js production
+
 ```
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,35 +2,28 @@ module.exports = {
   apps : [{
     name: 'ToyHomepage',
     script: 'server.js',
+    autorestart: true,
     watch: true,
     ignore_watch: ['node_modules', '.git', 'documents', ],
     env: {
+      name: 'production_page',
       NODE_ENV: 'production'
     }
   }],
 
   deploy : {
     production : {
+      key  : '/home/kweb/.ssh/id_rsa.pub',
       user : 'kweb',
       host : 'gch0123.iptime.org',
       ref  : 'origin/master',
+      // branch : 'master',
       repo : 'git@github.com:geatrigger/ToyHomepage.git',
-      path : '/var/www/production',
+      path : '/home/kweb/production',
+      // 'ssh_options': ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
-    },
-    staging : {
-      user : 'kweb',
-      host : 'gch0123.iptime.org',
-      ref  : 'origin/master',
-      repo : 'git@github.com:geatrigger/ToyHomepage.git',
-      path : '/var/www/development',
-      'ssh_options': ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
-      'post-deploy' : 'pm2 startOrRestart ecosystem.json --env dev',
-      'env'  : {
-        'NODE_ENV': 'staging'
-      }
     }
   }
 };
